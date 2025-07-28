315c87bce3bc31550d93559f261cd07b5f1eb006
praveen <tyagiboypraveen97@gmail.com>
2025-07-28 11:40:30 +0530

    new change

--- 01_create_experiment.py
+++ 01_create_experiment.py
@@ -19,6 +19,5 @@ if __name__ == "__main__":
     #create a new workflow experiment
     mlflow.create_experiment(
         name="testing_mlflow1",
-        artifact_location="testing_mlflow1_artifacts",
         tags={"env":"dev","version":"1.0.0"},
     )
